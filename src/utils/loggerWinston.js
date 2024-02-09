b6c0ed3306d133a786a544bb2337846f734aa7ed
Sai Rathod <rsaiprasad4@gmail.com>
2024-02-09 10:38:52 +0530

    fix: logger commented

--- src/utils/loggerWinston.js
+++ src/utils/loggerWinston.js
@@ -6,27 +6,27 @@
 
 // Require modules and functions
 const { createLogger, transports, format } = require("winston");
-const path = require("path");
+// const path = require("path");
 
-// Set the logging format.
-const myFormat = format.combine(
-  format.timestamp({
-    format: "YYYY-MM-DD hh:mm:ss A ZZ",
-  }),
-  format.json()
-);
+// // Set the logging format.
+// const myFormat = format.combine(
+//   format.timestamp({
+//     format: "YYYY-MM-DD hh:mm:ss A ZZ",
+//   }),
+//   format.json()
+// );
 
-// This logger is used to log the audit events (who did what and when)
-const _auditLogger = createLogger({
-  level: "info",
-  transports: [
-    new transports.File({
-      filename: path.join(process.env.LOG_DIRECTORY, "audit.log"),
-      maxsize: process.env.LOG_MAX_SIZE_BYTES,
-      format: myFormat,
-    }),
-  ],
-});
+// // This logger is used to log the audit events (who did what and when)
+// const _auditLogger = createLogger({
+//   level: "info",
+//   transports: [
+//     new transports.File({
+//       filename: path.join(process.env.LOG_DIRECTORY, "audit.log"),
+//       maxsize: process.env.LOG_MAX_SIZE_BYTES,
+//       format: myFormat,
+//     }),
+//   ],
+// });
 
 // This logger is used to log the audit events (who did what and when)
 // const _fcmMessageLogger = createLogger({
@@ -41,25 +41,27 @@ const _auditLogger = createLogger({
 // });
 
 // This logger is used to log for server activities. Explore more
-const _serverLogger = createLogger({
-  level: "info",
-  transports: [
-    new transports.File({
-      filename: path.join(process.env.LOG_DIRECTORY, "server.log"),
-      maxsize: process.env.LOG_MAX_SIZE_BYTES,
-      format: myFormat,
-      handleExceptions: true,
-    }),
-  ],
-});
+// const _serverLogger = createLogger({
+//   level: "info",
+//   transports: [
+//     new transports.File({
+//       filename: path.join(process.env.LOG_DIRECTORY, "server.log"),
+//       maxsize: process.env.LOG_MAX_SIZE_BYTES,
+//       format: myFormat,
+//       handleExceptions: true,
+//     }),
+//   ],
+// });
 
 
 const auditLogger = async (message) => {
-  _auditLogger.log("info", message);
+  // _auditLogger.log("info", message);
+  console.log("info", message)
 };
 
 const serverLogger = async (type, message) => {
-  _serverLogger.log(type, message);
+  // _serverLogger.log(type, message);
+  console.log(type, message)
 };
 
 
@@ -67,6 +69,6 @@ const serverLogger = async (type, message) => {
 // Export the logger functions
 module.exports = {
   auditLogger,
-//   fcmMessageLogger,
+  //   fcmMessageLogger,
   serverLogger,
 };
